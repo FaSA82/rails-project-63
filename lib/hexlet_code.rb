3187fe08acaa85a208cb1107096852b50e0ffc6d
Sergio <grey_f@inbox.ru>
2025-10-21 22:01:00 +0300

    Added: implementation of HTML tag generator

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,8 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
+  autoload :Tag, './tag'
   # Your code goes here...
 end
+
+puts HexletCode::Tag.build('img', src: 'path/to/image')
---
+++ lib/tag.rb
@@ -0,0 +1,7 @@
+module Tag
+    def self.build(name, attributes = {})
+    attrs = attributes.map{ |key, value| "#{key}=\"#{value}\""}.join(", ")
+    generated_tag = "<#{name} #{attrs}>"
+    generated_tag
+    end
+end
