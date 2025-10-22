80ab18b417f07f4b33f519891390200599f4915a
Sergio <grey_f@inbox.ru>
2025-10-23 01:18:52 +0300

    fix: relation between modules - absolute address added

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,7 +4,8 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  autoload :Tag, 'hexlet_code/tag.rb'
+  #autoload :Tag, 'hexlet_code/tag.rb'
+  autoload :Tag, File.expand_path('hexlet_code/tag', __dir__)
 end
 
 puts HexletCode::Tag.build('img', src: 'path/to/image')
